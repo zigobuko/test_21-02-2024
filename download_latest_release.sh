0d2a43cfe619933f6144415fa1e55f41047bf156
zigobuko <136098103+zigobuko@users.noreply.github.com>
2024-02-21 18:23:00 +0200

    Update download_latest_release.sh
    reverted to step where temp directory added

--- download_latest_release.sh
+++ download_latest_release.sh
@@ -4,8 +4,6 @@
 owner="zigobuko"
 repo="test_21-02-2024"
 
-
-
 # Get the latest release information
 release_info=$(curl -s "https://api.github.com/repos/$owner/$repo/releases/latest")
 
@@ -21,33 +19,29 @@ fi
 # Extract file name from the download URL
 filename=$(basename "$download_url")
 
-# Create a temporary directory for extraction
+# Download the zip file to the Downloads folder
+curl -sSL "$download_url" -o ~/Downloads/"$filename"
+
+# Unzip the downloaded file to a temporary directory
 temp_dir=$(mktemp -d)
+unzip -q ~/Downloads/"$filename" -d "$temp_dir"
 
-# Download the zip file to the temporary directory
-curl -sSL "$download_url" -o "$temp_dir/$filename"
+# Find the .app file
+app_file=$(find "$temp_dir" -name "*.app" -type f)
 
-# Unzip the downloaded file to the temporary directory, excluding macOS-specific metadata
-unzip -q -X -d "$temp_dir" "$temp_dir/$filename"
-
-# Check if an .app file with the same name already exists in Downloads
-app_file=$(find "$temp_dir" -name "*.app" -type f | head -n 1)
-if [ -n "$app_file" ]; then
-    app_filename=$(basename "$app_file")
-    if [ -e ~/Downloads/"$app_filename" ]; then
-        echo "An .app file with the same name already exists in the Downloads folder."
-        rm -rf "$temp_dir"
-        exit 1
-    fi
+# Check if .app file is found
+if [ -z "$app_file" ]; then
+    echo "No .app file found in the zip archive."
+    rm -rf "$temp_dir"  # Remove the temporary directory
+    rm ~/Downloads/"$filename"  # Remove the zip file
+    exit 1
 fi
-
-# Debug: Print the app file path
-echo "App file path: $app_file"
 
 # Move the .app file to the Downloads folder
 mv "$app_file" ~/Downloads/
 
-# Remove the temporary directory
+# Remove the temporary directory and the zip file
 rm -rf "$temp_dir"
+rm ~/Downloads/"$filename"
 
 echo "Downloaded and extracted successfully."
